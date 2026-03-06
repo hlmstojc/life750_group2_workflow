c8cc8146dd8162ceb19673ce587f98de0f161f14
hlmstojc <hlmstojc@liverpool.ac.uk>
2026-03-06 05:01:58 +0000

    Create 02-taxonomy.sh

---
+++ 02-taxonomy.sh
@@ -0,0 +1,7 @@
+kraken2 --paired \
+--db /mnt/hd/kraken2_db \
+--threads 8 \
+--output K1.kraken \
+--report K1.kreport2 \
+../poster_removeHost/K1_post_high_R1.final.fastq \
+../poster_removeHost/K1_post_high_R2.final.fastq
